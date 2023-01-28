317fdda52adb4d3be4ba4d6f4950d26026ef0fe6
Volodymyr Shvets <vshvets295@gmail.com>
2023-01-28 18:23:54 +0200

    First commit

---
+++ BusBookingSystem/src/test/java/com/booking/system/BusBookingSystemApplicationTests.java
@@ -0,0 +1,11 @@
+package com.booking.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusBookingSystemApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
