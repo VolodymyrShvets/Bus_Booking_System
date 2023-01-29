d234e43cbde75e0d9eafe9942f791c83f3694af4
Volodymyr Shvets <vshvets295@gmail.com>
2023-01-29 12:09:43 +0200

    Added logging to BusServiceImpl

--- BusBookingSystem/src/main/java/com/booking/system/service/BusServiceImpl.java
+++ BusBookingSystem/src/main/java/com/booking/system/service/BusServiceImpl.java
@@ -4,10 +4,12 @@ import com.booking.system.model.Bus;
 import com.booking.system.repository.BusRepository;
 import com.booking.system.service.api.BusService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class BusServiceImpl implements BusService {
@@ -15,31 +17,37 @@ public class BusServiceImpl implements BusService {
 
     @Override
     public Bus createNewBus(Bus bus) {
+        log.info("Creating new Bus with name {}", bus.getName());
         return repository.save(bus);
     }
 
     @Override
     public List<Bus> getBussesByArrivalCity(String arrivalCity) {
+        log.info("Receiving all Busses that arrive to {}", arrivalCity);
         return repository.findAllByArrivalCity(arrivalCity);
     }
 
     @Override
     public List<Bus> getBussesByDepartureCity(String departureCity) {
+        log.info("Receiving all Busses that departs from {}", departureCity);
         return repository.findAllByDepartureCity(departureCity);
     }
 
     @Override
     public List<Bus> getBussesByDepartureCityAndArrivalCity(String departureCity, String arrivalCity) {
+        log.info("Receiving all Busses that departs from {} and arrive to {}", departureCity, arrivalCity);
         return repository.findAllByDepartureCityAndArrivalCity(departureCity, arrivalCity);
     }
 
     @Override
     public Bus updateBus(Bus bus) {
+        log.info("Updating the Bus with name {}", bus.getName());
         return repository.save(bus);
     }
 
     @Override
     public void deleteBus(String busName) {
+        log.info("Deleting the Bus with name {}", busName);
         repository.deleteByName(busName);
     }
 }
