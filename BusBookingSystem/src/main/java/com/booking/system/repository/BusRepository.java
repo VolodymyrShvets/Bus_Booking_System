b9b084863e03196f147393d15abb0af214818934
Volodymyr Shvets <vshvets295@gmail.com>
2023-01-29 10:10:51 +0200

    Created Bus Repository

---
+++ BusBookingSystem/src/main/java/com/booking/system/repository/BusRepository.java
@@ -0,0 +1,17 @@
+package com.booking.system.repository;
+
+import com.booking.system.model.Bus;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BusRepository extends MongoRepository<Bus, String> {
+    List<Bus> findAllByArrivalCity(String arrivalCity);
+
+    List<Bus> findAllByDepartureCity(String departureCity);
+
+    List<Bus> findAllByDepartureCityAndArrivalCity(@Param("departureCity") String departureCity, @Param("arrivalCity") String arrivalCity);
+}
