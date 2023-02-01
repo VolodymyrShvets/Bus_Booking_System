6ca50b717cfb06e29a8cadb59b52fbeed77969b3
Volodymyr Shvets <vshvets295@gmail.com>
2023-02-01 19:06:50 +0200

    Updated annotations

--- BusBookingSystem/src/main/java/com/booking/system/model/Bus.java
+++ BusBookingSystem/src/main/java/com/booking/system/model/Bus.java
@@ -4,6 +4,8 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
@@ -14,9 +16,11 @@ import java.util.List;
 @Getter
 @EqualsAndHashCode
 @Component
+@Document
 public class Bus {
     @Id
     String id;
+    @Indexed(unique = true)
     String name;
     String departureCity;
     String arrivalCity;
