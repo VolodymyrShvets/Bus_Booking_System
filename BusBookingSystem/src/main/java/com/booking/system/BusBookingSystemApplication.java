848e051a76ac9bf54a9d0e2ca3dabb15784b15ea
Volodymyr Shvets <vshvets295@gmail.com>
2023-02-17 12:13:56 +0200

    Simplified CommandLineRunner and added couple TODO's

--- BusBookingSystem/src/main/java/com/booking/system/BusBookingSystemApplication.java
+++ BusBookingSystem/src/main/java/com/booking/system/BusBookingSystemApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class BusBookingSystemApplication {
@@ -15,10 +14,19 @@ public class BusBookingSystemApplication {
     }
 
     @Bean
-    public CommandLineRunner run(BusRepository repository, Environment env) {
+    public CommandLineRunner run(BusRepository repository) {
         return args -> {
-            Utility.saveBusses(repository, env.getProperty("busses.json.location"));
-            //Utility.saveBusses(repository, env.getProperty("busses-test.json.location")); // for tests
+            //Utility.saveBuses(repository, Utility.generateBuses());
         };
     }
 }
+// TODO create more buses with actual dates
+// TODO (after previous) add busses for one month prior
+// TODO (after previous) add search by dates
+// TODO (after previous) add deletion mechanism, if bus' arrival date is < than date.now
+// TODO (after previous) replace BusNotFoundException with message that no buses found
+// TODO add feature for user to change his info (name, last name, email, password)
+// TODO add feature for user to watch all his tickets
+// TODO (after previous) update ticket' status to EXPIRED if ticket arrival date is < than date.now
+// TODO (after previous) add deletion mechanism, is ticket is stored more than (1 month)
+// TODO add message about successful ticket creation and same for user creation
