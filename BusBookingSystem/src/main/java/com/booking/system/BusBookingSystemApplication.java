2bb760f3996b9ee9fb9732f8e2eca86a364af739
Volodymyr Shvets <vshvets295@gmail.com>
2023-02-20 18:33:13 +0200

    Updated TODO's

--- BusBookingSystem/src/main/java/com/booking/system/BusBookingSystemApplication.java
+++ BusBookingSystem/src/main/java/com/booking/system/BusBookingSystemApplication.java
@@ -17,13 +17,10 @@ public class BusBookingSystemApplication {
     public CommandLineRunner run(BusRepository repository) {
         return args -> {
             //Utility.saveBuses(repository, Utility.generateBuses());
+            Utility.trimBusCollection(repository);
         };
     }
 }
-// TODO create more buses with actual dates
-// TODO (after previous) add busses for one month prior
-// TODO (after previous) add search by dates
-// TODO (after previous) add deletion mechanism, if bus' arrival date is < than date.now
 // TODO (after previous) replace BusNotFoundException with message that no buses found
 // TODO add feature for user to change his info (name, last name, email, password)
 // TODO (after previous) update ticket' status to EXPIRED if ticket arrival date is < than date.now
