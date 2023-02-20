0eaa6976241d629c24985cb766d2d0b055f28fcf
Volodymyr Shvets <vshvets295@gmail.com>
2023-02-20 18:29:25 +0200

    Updated exception

--- BusBookingSystem/src/main/java/com/booking/system/model/exception/BusNotFoundException.java
+++ BusBookingSystem/src/main/java/com/booking/system/model/exception/BusNotFoundException.java
@@ -8,8 +8,4 @@ public class BusNotFoundException extends RuntimeException {
     public BusNotFoundException(String departure, String arrival) {
         super("Could not found Buses with Departure: " + departure + " and Arrival: " + arrival);
     }
-
-    public BusNotFoundException(int x, String place) {
-        super("Could not found Buses with " + (x == 1 ? "Departure: " : "Arrival: ") + place);
-    }
 }
