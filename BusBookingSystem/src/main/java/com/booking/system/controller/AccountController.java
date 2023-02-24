5316106fbf45eabf68cb722848d6a1d015d9ab0c
Volodymyr Shvets <vshvets295@gmail.com>
2023-02-24 15:48:25 +0200

    Updated account controller

--- BusBookingSystem/src/main/java/com/booking/system/controller/AccountController.java
+++ BusBookingSystem/src/main/java/com/booking/system/controller/AccountController.java
@@ -1,12 +1,11 @@
 package com.booking.system.controller;
 
 import com.booking.system.model.dto.TicketDTO;
+import com.booking.system.security.authentication.IAuthenticationFacade;
 import com.booking.system.service.api.EmailService;
 import com.booking.system.service.api.TicketService;
 import com.booking.system.utility.PdfUtil;
 import lombok.AllArgsConstructor;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,19 +14,20 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 @AllArgsConstructor
 public class AccountController {
+    private IAuthenticationFacade authenticationFacade;
     private TicketService ticketService;
     private EmailService emailService;
 
     @GetMapping(value = "/account/tickets")
     public String getUserTickets(Model model) {
-        String email = getAuthentication().getName();
+        String email = authenticationFacade.getAuthentication().getName();
         model.addAttribute("tickets", ticketService.getAllTicketsByUserEmail(email));
         return "account";
     }
 
     @GetMapping(value = "/account/ticket/{id}")
     public String sendTicketToEmail(@PathVariable String id) {
-        String email = getAuthentication().getName();
+        String email = authenticationFacade.getAuthentication().getName();
         String subject = "Bus Ticket";
         String text = "Your bus ticket. Thanks for using our system.";
 
@@ -49,13 +49,9 @@ public class AccountController {
 
     @GetMapping(value = "/account/tickets/delete/{busName}/{seat}")
     public String deleteTicket(@PathVariable String busName, @PathVariable String seat, Model model) {
-        String email = getAuthentication().getName();
+        String email = authenticationFacade.getAuthentication().getName();
         ticketService.deleteTicket(busName, Long.parseLong(seat));
         model.addAttribute("tickets", ticketService.getAllTicketsByUserEmail(email));
         return "account";
-    }
-
-    public Authentication getAuthentication() {
-        return SecurityContextHolder.getContext().getAuthentication();
     }
 }
