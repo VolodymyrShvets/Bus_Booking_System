92a4999df55feb4c90ca1d28e9a5d38892a22a83
Volodymyr Shvets <vshvets295@gmail.com>
2023-04-14 20:17:53 +0300

    Moved bCryptPasswordEncoder() bean to Config class

--- BusBookingSystem/src/main/java/com/booking/system/config/Config.java
+++ BusBookingSystem/src/main/java/com/booking/system/config/Config.java
@@ -3,11 +3,17 @@ package com.booking.system.config;
 import com.booking.system.model.dto.TicketDTO;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class Config {
     @Bean
     public TicketDTO ticketDTO() {
         return new TicketDTO();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder(11);
     }
 }
--- BusBookingSystem/src/main/java/com/booking/system/config/SpringSecurityConfig.java
+++ BusBookingSystem/src/main/java/com/booking/system/config/SpringSecurityConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -40,10 +39,5 @@ public class SpringSecurityConfig {
                 .permitAll()
                 .logoutSuccessUrl("/?logout=true");
         return http.build();
-    }
-
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() { // TODO move this bean to Config class
-        return new BCryptPasswordEncoder(11);
     }
 }
