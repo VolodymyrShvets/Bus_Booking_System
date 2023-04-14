24ef9175948137cb1aacfacc6bc240944ee522d6
Volodymyr Shvets <vshvets295@gmail.com>
2023-04-14 20:21:33 +0300

    Moved email properties to .properties file

--- BusBookingSystem/src/main/java/com/booking/system/config/EmailConfiguration.java
+++ BusBookingSystem/src/main/java/com/booking/system/config/EmailConfiguration.java
@@ -1,28 +1,33 @@
 package com.booking.system.config;
 
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.core.env.Environment;
 
 import java.util.Properties;
 
 @Configuration
+@AllArgsConstructor
 public class EmailConfiguration {
+    private Environment env;
+
     @Bean
     public JavaMailSender getJavaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.gmail.com");
-        mailSender.setPort(587);
+        mailSender.setHost(env.getProperty("mail.host"));
+        mailSender.setPort(Integer.parseInt(env.getProperty("mail.port")));
 
-        mailSender.setUsername("shvetsasia172@gmail.com");
-        mailSender.setPassword("roby tsfj mitb yvni");
+        mailSender.setUsername(env.getProperty("mail.username"));
+        mailSender.setPassword(env.getProperty("mail.password"));
 
         Properties props = mailSender.getJavaMailProperties();
-        props.put("mail.transport.protocol", "smtp");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        //props.put("mail.debug", "true");
+        props.put("mail.transport.protocol", env.getProperty("mail.transport.protocol"));
+        props.put("mail.smtp.auth", env.getProperty("mail.smtp.auth"));
+        props.put("mail.smtp.starttls.enable", env.getProperty("mail.smtp.starttls.enable"));
+        //props.put("mail.debug", env.getProperty("mail.debug"));
 
         return mailSender;
     }
