43481a4b1bfa7a68a066e36ac28e4f7cca978a5b
Volodymyr Shvets <vshvets295@gmail.com>
2023-04-14 20:02:01 +0300

    Updated advice classes

--- BusBookingSystem/src/main/java/com/booking/system/controller/advice/UserAlreadyExistsAdvice.java
+++ BusBookingSystem/src/main/java/com/booking/system/controller/advice/UserAlreadyExistsAdvice.java
@@ -1,18 +1,21 @@
 package com.booking.system.controller.advice;
 
 import com.booking.system.model.exception.UserAlreadyExistsException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+@Slf4j
 @ControllerAdvice
 public class UserAlreadyExistsAdvice {
     @ResponseBody
     @ExceptionHandler(UserAlreadyExistsException.class)
     @ResponseStatus(HttpStatus.CONFLICT)
     String userAlreadyExistsHandler(UserAlreadyExistsException ex) {
-        return ex.getMessage();
+        log.error("Following exception occurred {}: {}", ex.getClass(), ex.getMessage());
+        return "error";
     }
 }
